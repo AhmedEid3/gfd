75a086ea728115fc8fb3c66b411e1eb4cd32534a
AhmedEid3 <ahmed.eid3@outlook.com>
2019-10-18 22:48:12 +0200

    fix issue on mobile device

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 
 $(document).ready(function () {
     // Init Nice Scroll
-    $("body").niceScroll({
-        cursorcolor: "#2FCCF6",
-        cursorborder: "1px solid #2FCCF6",
-        cursorwidth: "10px",
-        cursorborderradius: "3px",
-        cursorminheight: "64"
-    });
+    // $("body").niceScroll({
+    //     cursorcolor: "#2FCCF6",
+    //     cursorborder: "1px solid #2FCCF6",
+    //     cursorwidth: "5px",
+    //     cursorborderradius: "3px",
+    //     cursorminheight: "64"
+    // });
 
     // Init Smooth Scroll
     var scroll = new SmoothScroll('a[href*="#"]');
